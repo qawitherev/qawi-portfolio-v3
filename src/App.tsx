f9862a2a94a8e1cac652e9fdd52faeb0268130a4
Abdul Qawi Kamran <qawitherev@gmail.com>
2024-04-29 15:15:19 +0800

    initial commit! + initial layout non-responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='flex flex-row h-screen' >
+      <div className='w-2/12 bg-blue-500'>
+        
+      </div>
+      <div className='w-8/12 bg-blue-300'></div>
+      <div className='w-2/12 bg-blue-500'></div>
     </div>
+    </>
   );
 }
 
